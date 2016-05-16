406a1d5b985ee85a139eb11aee8aa6fc090e7fe9
Matheus Figueredo <matheus.figueredo@gmail.com>
2016-05-16 20:16:22 -0300

    add script to install caffe framework

--- setup.sh
+++ setup.sh
@@ -7,9 +7,12 @@ sudo apt-get -y install vim apache2 libapache2-mod-wsgi python-pip
 # Installing all requirements projects
 sudo pip install -r /var/www/wearn/requirements.txt
 
-# Create site in apacha
+# Create site in apache
 cd /etc/apache2
 sudo a2dissite 000-default
 sudo cp /var/www/wearn/wearn.conf sites-available/
 sudo a2ensite wearn.conf
 sudo service apache2 reload
+
+# For install caffe framework
+sudo chmod +x /var/www/wearn/caffe/setup.sh
