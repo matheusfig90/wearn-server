cd7e79dc835f8f34c82f8b1d6bc2bd902259c2b7
Matheus Figueredo <matheus.figueredo@gmail.com>
2016-10-06 20:16:33 -0300

    implements script to get all wears by netshoes

---
+++ scripts/netshoes.py
@@ -0,0 +1,50 @@
+# Dependencies
+import urllib2
+
+from bs4 import BeautifulSoup
+from wearn import app
+from wearn.models.wear import Wear
+
+# Set URLs to scrapping
+urls = [
+    # Men
+    'http://www.netshoes.com.br/camisas-polo/masculino?mi=hm_ger_mntop_H-ROU-camisas-polo&psn=Menu_Top&fc=menu',
+    'http://www.netshoes.com.br/camisetas/masculino?mi=hm_ger_mntop_H-ROU-camisetas&psn=Menu_Top&fc=menu',
+
+
+    # Women
+    'http://www.netshoes.com.br/camisas-polo/feminino?mi=hm_ger_mntop_M-ROU-camisas-polo&psn=Menu_Top&fc=menu',
+    'http://www.netshoes.com.br/camisetas/feminino?mi=hm_ger_mntop_M-ROU-camisetas&psn=Menu_Top&fc=menu'
+]
+
+# Each in all urls
+for url in urls:
+    currentUrl = url
+
+    while True:
+        # Load page
+        soup = BeautifulSoup(urllib2.urlopen(currentUrl))
+
+        # Get all images
+        for item in soup.select('.product-list-item'):
+            try:
+                # Get image
+                image_link = item.select('.product-img')[0].select('img')[0].attrs.get('data-src')
+                image_link = 'http:' + image_link.split('?')[0].replace('detalhe1.jpg', 'zoom1.jpg')
+
+                Wear.save({
+                    'name': item.select('.name')[0].get_text(),
+                    'image': image_link,
+                    'price': 0.00,
+                    'link': ('http://www.netshoes.com.br' + item.select('a')[0].attrs.get('href')),
+                })
+            except:
+                pass
+    
+        if len(soup.select('.pagination-link.is-next')) == 0:
+            break
+
+        currentUrl = 'http://www.netshoes.com.br' + soup.select('.pagination-link.is-next')[0].attrs.get('href')
+
+
+
--- wearn/models/wear.py
+++ wearn/models/wear.py
@@ -4,6 +4,8 @@ import cPickle as pickle
 import cv2
 import numpy as np
 import os
+import random
+import urllib
 
 from scipy.spatial.distance import cosine, correlation
 from wearn import app
@@ -24,13 +26,19 @@ class Wear(object):
     arch  = os.path.dirname(os.path.abspath(__file__)) + '/../../caffe/deploy.prototxt'
     model = os.path.dirname(os.path.abspath(__file__)) + '/../../caffe/bvlc_googlenet.caffemodel'
 
+    # Set caffe network
+    net = caffe.Net(arch, model, caffe.TEST)
+
+    # Set net layer
+    layer = 'pool5/7x7_s1'
+
     """
         Search wears by image.
 
         Returns a prediction array
     """
     @staticmethod
-    def search( image):
+    def search(image):
         # Set caffe network
         net = caffe.Net(Wear.arch, Wear.model, caffe.TEST)
 
@@ -50,7 +58,7 @@ class Wear(object):
         net.forward()
 
         # Get feature maps from image
-        feature_maps = net.blobs['pool5/7x7_s1'].data
+        feature_maps = net.blobs[Wear.layer].data
 
         # Searching in database
         wears = Database.connect().wears.find({})
@@ -60,7 +68,7 @@ class Wear(object):
 
         for wear in wears:
             # Get feature maps from wear
-            wear_fm = pickle.loads(str(wear['feature_maps']))['wearn']
+            wear_fm = pickle.loads(str(wear['feature_maps']))['_feature_maps']
 
             """ Calculating distances """
         
@@ -76,3 +84,43 @@ class Wear(object):
             predictions.append([str(wear['name']), str(wear['image']), str(wear['link']), euclidean_distance])
 
         return predictions
+
+    """
+        Save wear
+
+        Returns new id
+    """
+    @staticmethod
+    def save(args):
+        if app.config['DEBUG'] == True:
+            print 'Download image from ' + str(args['image'])
+        
+        # Download image
+        image_file = '/tmp/' + str(random.randint(1,999999)) + '.jpg'
+        urllib.urlretrieve(args['image'], image_file)
+
+        # Read image and convert to matrix
+        image_matrix = cv2.imread(image_file)
+        image_matrix = cv2.resize(image_matrix, (224, 224)) # Resize image to 224x224
+        image_matrix = image_matrix[:, :, ::-1] # Convert BGR to RGB
+
+        image_data = image_matrix.astype(np.float32)
+
+        # Resize image
+        Wear.net.blobs["data"].reshape(1, 3, 224, 224)
+
+        image_data = image_data.transpose([2, 0, 1])
+
+        Wear.net.blobs["data"].data[...] = image_data
+        Wear.net.forward()
+
+        # Serialize feature maps to save in database
+        feature_maps = pickle.dumps({ '_feature_maps': Wear.net.blobs[Wear.layer].data })
+
+        return Database.connect().wears.insert_one({ 
+            'name': args['name'],
+            'image': args['image'],
+            'price': float(args['price']),
+            'link': args['link'],
+            'feature_maps': feature_maps
+        })
